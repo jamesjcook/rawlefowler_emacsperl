f41b144d629e091a1c3f3f894b0bb9d93fb345ef
James Cook <jamesjcook@users.noreply.github.com>
2024-03-12 17:41:26 -0400

    Moved the theme setup outside of init.el so that init.el could be re-used as part of greater mess of emacs init

--- init.el
+++ init.el
@@ -109,23 +109,6 @@
 (global-set-key (kbd "C-c C-c") #'kill-inner-word)
 (global-set-key (kbd "M-x") #'smex)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(dracula))
- '(custom-safe-themes
-   '("fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" default))
- '(delete-selection-mode nil)
- '(package-selected-packages '(flycheck dracula-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;(provide 'init)
 
 ;;; init.el ends here.
